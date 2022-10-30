a3da72fdbb461a2b2a19efb257f483e19b2f9419
Dohyun1225 <101166157+Dohyun1225@users.noreply.github.com>
2022-10-30 23:30:12 +0900

    common

---
+++ AppSW/Screen/MainMenu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, Button } from 'react-native';
+
+// 메인 메뉴로 활용될 씬입니다.
+
+    const MainMeun = (props) => {
+        return(
+            <View>
+            <Text> MainMeun </Text>
+            </View>
+        );
+    };
+
+
+export default MainMeun;
